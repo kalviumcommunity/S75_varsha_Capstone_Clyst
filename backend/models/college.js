c169d02e414406c33af0858c97061e7f64882c4a
varsha479 <varsha.k.s75@kalvium.community>
2025-07-07 17:43:10 +0530

    Update college.js
    Temporary removal of schema to re add with pr

--- backend/models/college.js
+++ backend/models/college.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const collegeSchema = new mongoose.Schema({
-  name: String,
-  location: String,
-  courses: [String],
-  rating: Number,
-});
-
-module.exports = mongoose.model("College", collegeSchema);
